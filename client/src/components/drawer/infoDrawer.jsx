7871b06fc1fe1c47b4a1b088c57d4a404cc7b873
Bedprakash <81341809+bedprakash0562@users.noreply.github.com>
2023-05-04 20:59:55 +0530

    my whatsapp clone

---
+++ client/src/components/drawer/infoDrawer.jsx
@@ -0,0 +1,26 @@
+// import { Drawer } from '@mui/material';
+
+// const drawerStyle = {
+//   left: 20,
+//   top: 17
+// }
+
+// const InfoDrawer = ({ open, setOpen }) => {
+//   const handleClose = () => {
+//     setOpen(false);
+//   }
+
+//   return (
+//     <Drawer
+//       open={open}
+//       onClose={handleClose}
+//       PaperProps={{ sx: drawerStyle }}
+//       style={{ zIndex: 1500 }}
+//     >
+//       {/* Content of the drawer goes here */}
+//       {/* For example: */}
+//       <h2>Drawer content</h2>
+//       <p>This is a drawer component.</p>
+//     </Drawer>
+//   );
+// };
